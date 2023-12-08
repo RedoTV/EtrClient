c5114bfdcd193a1fd1c19db70dbf4aed65f3a5b9
deorizz <deorix@firemail.cc>
2023-12-08 21:35:06 +0300

    Re-commit

--- src/app/components/contests/contests.component.ts
+++ src/app/components/contests/contests.component.ts
@@ -24,18 +24,92 @@ export class ContestsComponent implements OnInit{
 
   ngOnInit(): void {
     this.width = document.body.clientWidth;
+    this.sortContestsByDate(-1);
   }
 
   @HostListener('window:resize', ['$event'])
-  onResize(event : Event) {
-    console.log((event.target as Window).innerWidth);
-    this.ngOnInit();
-  }
-  
-  reloadCurrentRoute() {
-    this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
-      this.router.navigate([this.router.url]);
-    });
+  onResize() {
+    this.width = document.body.clientWidth;
   }
 
+  sortTable(event : Event) {
+    var sortButtons = document.getElementsByClassName("table-sort-button");
+    var clickedButton = event.target as Element;
+    console.log(clickedButton);
+    for (var i = 0; i < sortButtons.length; i++)
+    {
+      if (sortButtons.item(i)?.id == clickedButton.id)
+      {
+        if (clickedButton.children.item(0)?.className == "sort-start-higher table-sort-button-content")
+        {
+          this.assertSortByElementId(-1, clickedButton);
+        }
+        else
+        {
+          /*
+          clickedButton.children.item(0)!.className = "sort-start-higher table-sort-button-content";
+          if (clickedButton.id == "sort-by-ID")
+          {
+            this.contests.sort((a, b) => b.id - a.id);
+            return;
+          }
+          */
+          this.assertSortByElementId(1, clickedButton);
+        }
+      }
+      else if (sortButtons.item(i)!.children.item(0)!.className != "table-sort-button-content")
+      {
+        sortButtons.item(i)!.children.item(0)!.className = "table-sort-button-content";
+      }
+    }
+  }
+
+  private assertSortByElementId(sortDirection : number, buttonElement : Element) {
+    if (sortDirection == 1)
+    {
+      buttonElement.children.item(0)!.className = "sort-start-higher table-sort-button-content";
+    }
+    else if (sortDirection == -1)
+    {
+      buttonElement.children.item(0)!.className = "sort-start-lower table-sort-button-content";
+    }
+    else
+      return;
+
+    if (buttonElement.id == "sort-by-ID")
+    {
+      this.contests.sort((a, b) => (a.id - b.id)*sortDirection);
+      return;
+    }
+    else if (buttonElement.id == "sort-by-name")
+    {
+      this.contests.sort((a, b) => {
+        if (a.name > b.name)
+          return sortDirection;
+        else if (a.name < b.name)
+          return sortDirection * -1;
+        return 0;
+      });
+      return;
+    }
+    else if (buttonElement.id == "sort-by-date")
+    {
+      this.sortContestsByDate(sortDirection);
+      return;
+    }
+  }
+
+  private sortContestsByDate (sortDirection : number) {
+    this.contests.sort((a, b) => new Date(
+      Number.parseInt(a.start_datatime.split(' ')[2]), 
+      Number.parseInt(a.start_datatime.split(' ')[1]),
+      Number.parseInt(a.start_datatime.split(' ')[0])
+      ).valueOf() * sortDirection * -1 - new Date(
+      Number.parseInt(b.start_datatime.split(' ')[2]), 
+      Number.parseInt(b.start_datatime.split(' ')[1]),
+      Number.parseInt(b.start_datatime.split(' ')[0])
+      ).valueOf()
+    );
+    return;
+  }
 }
