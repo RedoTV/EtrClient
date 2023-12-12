0f9690b0724144e55fb0a047bb96c0fc144edad8
deorizz <deorix@firemail.cc>
2023-12-12 11:34:34 +0300

    Made routerLink linking contest participants and their pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -33,7 +33,7 @@ export const routes: Routes = [
         component: AboutComponent
     },
     {
-        path: 'student/:id',
+        path: 'students/:id',
         component: StudentViewComponent
     }
 ];
