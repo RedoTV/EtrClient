9ea68228b5970258a3baead66eeedeb219088618
FillOff <105561203+FillOff@users.noreply.github.com>
2023-12-21 19:18:15 +0300

    fotter is dead. it's nice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
-import { FooterComponent } from "./components/footer/footer.component";
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
@@ -10,7 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HttpClientModule]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, HttpClientModule]
 })
 export class AppComponent {
   title = 'EtrClient';
