18642527854510bbedb49a49b6e1a55505e649a4
deorizz <deorix@firemail.cc>
2024-01-07 13:55:52 +0300

    Resolved some problems with import in app routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,8 +7,8 @@ import { AboutComponent } from './components/about/about.component';
 import { ContestViewComponent } from './components/contest-view/contest-view.component';
 import { StudentViewComponent } from './components/student-view/student-view.component';
 import { ProblemComponent } from './components/problem/problem.component';
-import { ExternalLinkComponent } from './components/external-link/external-link.component';
 import { HomeComponent } from './components/home/home.component';
+import { ExternalLinkComponent } from './components/external-link/external-link.component';
 
 export const routes: Routes = [
     {
