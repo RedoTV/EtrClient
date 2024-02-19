aa47c3ce1ce3d87cb7e09ca9e852b7b8fd08bf52
FillOff <filloff2022@outlook.com>
2024-02-19 18:37:51 +0300

    problem tree remake

--- src/app/components/problems-tree/problems-tree.component.ts
+++ src/app/components/problems-tree/problems-tree.component.ts
@@ -4,42 +4,27 @@ import { Problem } from '../../models/request.problem';
 import { ProblemsService } from '../../services/problems.service';
 import { Subject, Subscription } from 'rxjs';
 import { RouterModule } from '@angular/router';
-import { TableTemplateNewComponent, TableData, TableRow } from '../table-template-new/table-template-new.component';
 
 @Component({
   selector: 'app-problems-tree',
   standalone: true,
-  imports: [CommonModule, RouterModule, TableTemplateNewComponent],
+  imports: [CommonModule, RouterModule],
   templateUrl: './problems-tree.component.html',
   styleUrl: './problems-tree.component.css'
 })
 export class ProblemsTreeComponent implements OnDestroy {
   problems: Problem[] = [];
-  problemTableSub: Subscription; 
+  problemTableSub: Subscription;
   tags: Set<string> = new Set<string>;
-
-  formattedTableData : TableData = new TableData;
-  filteredTableData : TableData = new TableData;
-  refreshTable : Subject<boolean> = new Subject<boolean>();
 
   constructor(private problemService : ProblemsService)
   {
-    this.formattedTableData.tableColNames = ["ID", "Название", "Теги"];
-    this.formattedTableData.colSortableFlag = [true, true, false];
-    this.formattedTableData.ignoreSortingFlags = [false];
 
     this.problemTableSub = this.problemService.getAllProblems()
     .subscribe(res => {
 
       res.forEach(x => this.problems.push(x));
       this.problems.forEach(problem => {
-        let newTableRow = new TableRow;
-        newTableRow.contents = [
-          problem.id,
-          problem.name,
-          problem.tags
-        ];
-
         problem.tags.forEach(tag => {
           let newTag = tag[0].toUpperCase();
           for (let i = 1; i < tag.length; i++) 
@@ -48,37 +33,18 @@ export class ProblemsTreeComponent implements OnDestroy {
           }
           this.tags.add(newTag);
         })
-
-
-        let tagsHtml = "";
-          if (problem.tags.length != 0)
-          {
-            const tagHtmlTemplate = '<div style="background-color: #00000008; border: 1.5px solid gray; text-wrap:nowrap; font-size: 18px; line-height: 16px; height: 26px; padding: 4px; margin: 4px; float: left; border-radius: 10px;">'
-            tagsHtml = '<div style="max-width: 400px;">\n' + tagHtmlTemplate;
-            tagsHtml += problem.tags.join('</div>\n'+ tagHtmlTemplate);
-            tagsHtml += '</div>\n</div>';
+        for (let i = 0; i < this.problems.length-1; i++) {
+          for (let j = 0; j < this.problems.length-1-i; j++) {
+            if (this.problems[j].tags.length > this.problems[j+1].tags.length)
+            {
+              var problem: Problem;
+              problem = this.problems[j];
+              this.problems[j] = this.problems[j+1];
+              this.problems[j+1] = problem;
+            }
           }
-
-          let externalUrl : string = "";
-          if (problem.contest_id !== null && problem.contest_id < 10000) {
-            externalUrl = `https://codeforces.com/problemset/problem/${problem.contest_id}/${problem.index}`
-          }
-          else if (problem.contest_id !== null && problem.contest_id >= 10000) {
-            externalUrl = `https://codeforces.com/gym/${problem.contest_id}/problem/${problem.index}`
-          }
-
-          newTableRow.routerLink = externalUrl;
-          this.formattedTableData.tableRows.push(newTableRow);
+        }
       });
-
-      this.filteredTableData.tableRows = JSON.parse(JSON.stringify(this.formattedTableData.tableRows));
-      this.filteredTableData.colSortableFlag = JSON.parse(JSON.stringify(this.formattedTableData.colSortableFlag));
-      this.filteredTableData.directionPresets = JSON.parse(JSON.stringify(this.formattedTableData.directionPresets));
-      this.filteredTableData.tableColNames = JSON.parse(JSON.stringify(this.formattedTableData.tableColNames));
-      this.filteredTableData = this.filteredTableData;
-
-      //перезагружаем таблицу, чтобы все данные отобразились 
-      this.refreshTable.next(true);
     });
   }
 
@@ -91,11 +57,33 @@ export class ProblemsTreeComponent implements OnDestroy {
     }
 
   }
-
-  contains(problemTags: TableRow, tag: string) {
-    return problemTags.contents[2].indexOf(tag) != -1;
+  contains(problem: Problem, tag: string) {
+    return problem.tags.indexOf(tag) != -1;
   }
-
+  SortByTagsAmount(problems: Problem[]) {
+    for (let i = 0; i < this.problems.length-1; i++) {
+      for (let j = 0; j < this.problems.length-1-i; j++) {
+        if (this.problems[j].tags.length < this.problems[j+1].tags.length)
+        {
+          var problem: Problem;
+          problem = this.problems[j];
+          this.problems[j] = this.problems[j+1];
+          this.problems[j+1] = problem;
+        }
+      }
+    }
+  }
+  RouterLink(problem: Problem)
+  {
+    let externalUrl : string = "";
+    if (problem.contest_id !== null && problem.contest_id < 10000) {
+      externalUrl = `https://codeforces.com/problemset/problem/${problem.contest_id}/${problem.index}`
+    }
+    else if (problem.contest_id !== null && problem.contest_id >= 10000) {
+      externalUrl = `https://codeforces.com/gym/${problem.contest_id}/problem/${problem.index}`
+    }
+    return externalUrl;
+  }
   ngOnDestroy() {
     this.problemTableSub.unsubscribe();
   }
